25ffd4cd05581fe9b20725c0abc121fd0676ee94
Reza <reza.bakhshi@lifelenz.com>
2022-12-06 11:35:40 +1030

    style classes does not work on content inside contenttemplate

--- MauiAppNew/SubContent.xaml.cs
+++ MauiAppNew/SubContent.xaml.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 namespace MauiAppNew;
 
 public partial class SubContent : Grid
@@ -5,23 +6,42 @@ public partial class SubContent : Grid
 	public SubContent()
 	{
 		InitializeComponent();
-	}
+        Loaded += (sender, args) => StyleClass = new List<string> { "blue" };
+    }
 
     private int count = 0;
+
+    private IList<T> FindAll<T>()
+    where T:IVisualTreeElement
+    {
+        return this.GetVisualTreeDescendants()
+            .Where(e => e is T)
+            .Cast<T>()
+            .ToList();
+    }
 
     private void Button_OnClicked(object sender, EventArgs e)
     {
         count++;
-        this.StyleClass ??= new List<string>();
+        var findAll = FindAll<VisualElement>();
+
         if (count % 2 == 1)
         {
-            this.StyleClass = new List<string> { "red" };
-            // VisualStateManager.GoToState(this, VisualStateManager.CommonStates.Selected);
+            // this.StyleClass = new List<string> { "red" };
+            VisualStateManager.GoToState(this, VisualStateManager.CommonStates.Selected);
+            foreach (var label in findAll)
+            {
+                VisualStateManager.GoToState(label, VisualStateManager.CommonStates.Selected);
+            }
         }
         else
         {
-            // VisualStateManager.GoToState(this, VisualStateManager.CommonStates.Normal);
-            this.StyleClass = new List<string> { "blue" };
+            VisualStateManager.GoToState(this, VisualStateManager.CommonStates.Normal);
+            foreach (var label in findAll)
+            {
+                VisualStateManager.GoToState(label, VisualStateManager.CommonStates.Normal);
+            }
+            //this.StyleClass = new List<string> { "blue" };
 
             /*this.StyleClass.Remove("red");
             this.StyleClass.Add("blue");*/
