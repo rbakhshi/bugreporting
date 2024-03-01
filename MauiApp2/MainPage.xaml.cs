4385d2bd5fe040cb5bf4f390d535a39e3556930a
Reza@mac <reza.bakhshi@lifelenz.com>
2024-03-01 22:27:40 +1030

    sample use of bcrypt.net which crashes if AOT-ed

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        var w = Stopwatch.StartNew();
+        BCrypt.Net.BCrypt.Verify("111111", "$2a$12$luf9xtzcPijRzyMnb1PxsuqFUsBba0ve.R.5k00XOsGf2awcHwj8a");
+        w.Stop();
+        Debug.WriteLine(w.Elapsed);
+        
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time {w.Elapsed}";
+        else
+            CounterBtn.Text = $"Clicked {count} times {w.Elapsed}";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
